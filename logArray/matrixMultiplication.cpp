a889766a7d9ce62538face512cd229288bedbd8a
Tijesunimi Peters <tijesunimipeters@gmail.com>
2020-04-26 03:33:25 -0500

    Done with the matrix multiplication

--- logArray/matrixMultiplication.cpp
+++ logArray/matrixMultiplication.cpp
@@ -6,17 +6,22 @@
 #define n 1024
 
 int matrixA[n][n], matrixB[n][n], matrixC[n][n];
+double parTime;
+omp_lock_t locks[n];
 
-void timer(void (*func)(int *a, int *b, int *c, int limit), int *a, int *b, int *c, int limit) {
+double timer(void (*func)(int *a, int *b, int *c, int limit), int *a, int *b, int *c, int limit) {
   clock_t startTime = omp_get_wtime();
   func(a, b, c, limit);
-  printf("Completed in %f\n", omp_get_wtime() - startTime);
+  double elapsed = omp_get_wtime() - startTime;
+  printf("Completed:\t\t%f seconds\n", elapsed);
+  return elapsed;
 }
 
 void runSequential(int *a, int *b, int *c, int limit) {
   for(int i = 0; i < limit; i++) {
     for(int j = 0; j < limit; j++) {
       for(int k = 0; k < limit; k++) {
+        // Don't understand why using the pointers runs super fast
         // (&c[i])[j] += (&a[i])[k] * (&b[k])[j];
         matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
       }
@@ -42,7 +47,9 @@ void runParallel(int *a, int *b, int *c, int limit) {
     for(int j = 0; j < limit; j++) {
       for(int k = 0; k < limit; k++) {
         // (&c[i])[j] += (&a[i])[k] * (&b[k])[j];
+        omp_set_lock(&locks[i]);
         matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+        omp_unset_lock(&locks[i]);
       }
     }
   }
@@ -55,7 +62,9 @@ void reversedParallel(int *a, int *b, int *c, int limit) {
     for(int k = 0; k < limit; k++) {
       for(int j = 0; j < limit; j++) {
         // (&c[i])[j] += (&a[i])[k] * (&b[k])[j];
+        omp_set_lock(&locks[i]);
         matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+        omp_unset_lock(&locks[i]);
       }
     }
   }
@@ -90,6 +99,13 @@ void reset(int *arr, int limit) {
   }
 }
 
+void printSpeedUp(double seqTime, double parTime) {
+  double speedUp = seqTime/parTime;
+  double processorUtil = speedUp / omp_get_num_procs();
+
+  printf("Speed Up:\t\t%f times\nProcessor Util:\t\t%f\n", speedUp, processorUtil);
+}
+
 int main() {
   // initializeData(*matrixA, n);
   // initializeData(*matrixB, n);
@@ -99,28 +115,39 @@ int main() {
       matrixA[i][j] = rand() % 10;
       matrixB[i][j] = rand() % 10;
     }
+
+    omp_init_lock(&locks[i]);
   }
 
+  // Using locks is slower than not using locks in the parallel execution
+
   printf("Sequential\n=====================\n");
-  timer(runSequential, *matrixA, *matrixB, *matrixC, n);
+  double seqTime = timer(runSequential, *matrixA, *matrixB, *matrixC, n);
   // runSequential(*matrixA, *matrixB, *matrixC, n);
   
   printf("\n");
   reset(*matrixC, n);
   printf("Reversed Sequential\n=====================\n");
-  timer(reversedSequential, *matrixA, *matrixB, *matrixC, n);
+  parTime = timer(reversedSequential, *matrixA, *matrixB, *matrixC, n);
+  printSpeedUp(seqTime, parTime);
   // reversedSequential(*matrixA, *matrixB, *matrixC, n);
 
   printf("\n");
   reset(*matrixC, n);
   printf("Parallel\n=====================\n");
-  timer(runParallel, *matrixA, *matrixB, *matrixC, n);
+  parTime = timer(runParallel, *matrixA, *matrixB, *matrixC, n);
+  printSpeedUp(seqTime, parTime);
   // runParallel(*matrixA, *matrixB, *matrixC, n);
 
   printf("\n");
   reset(*matrixC, n);
   printf("Reversed Parallel\n=====================\n");
-  timer(reversedParallel, *matrixA, *matrixB, *matrixC, n);
+  parTime = timer(reversedParallel, *matrixA, *matrixB, *matrixC, n);
+  printSpeedUp(seqTime, parTime);
+
+  for(int i = 0; i < n; i++) {
+    omp_destroy_lock(&locks[i]);
+  }
   // reversedParallel(*matrixA, *matrixB, *matrixC, n);
 
   // printMatrix(*matrixA, 'A');
